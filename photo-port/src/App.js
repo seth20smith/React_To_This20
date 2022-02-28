0c4a914cd8436fe1381742266383924aaf95d32a
Smith1 <seth20smith@gmail.com>
2022-02-27 19:26:29 -0800

    just about finished

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import img_weather from "https://user-images.githubusercontent.com/91171134/141701804-bfd78a06-023b-4b36-8540-71c59fe8d803.png";
+import img_planner from "https://courses.bootcampspot.com/users/12064/files/1042446/preview?verifier=N5fY1wXjNY0ZpBjivqmF7jtSbF3IUadFDph6Deuo";
+import img_noteTaker from "https://user-images.githubusercontent.com/91171134/155905797-f77aee12-44c7-49bb-9289-57356161f71b.png";
+// import { Route, Switch, BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <header id="headerImg">
+    <nav>
+      <h1>
+        Portfolio-For-Pros
+      </h1>
+    </nav>
+    <nav>
+      <ul>
+        <li>
+          <a href="#About Me">About Me</a>
+        </li>
+        <li>
+          <a href="#Work">Work</a>
+        </li>
+        <li>
+          <a href="#GitHub">Resume</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul> 
+    </nav>
+  </header>
+  {/* Header ends */}
+  {/* Body Starts here formatt */}
+  {/* About me*/}
+  <section className="About Me">
+    <h3 id="About Me">About Me</h3>
+    <div className="About-Me-Block-1">
     </div>
+  </section>
+  <div className="container">
+    <section className="building-blocks">
+      <div className="building-block-1 left-side">
+        <h3>Student Coder</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi ea at omnis asperiores, voluptas veniam voluptatem temporibus quaerat deleniti dolore, magnam fuga quae animi iste quis sed ipsa autem accusantium.</p>
+      </div>
+      <div className="building-block-2">
+        <h3>Goals</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ducimus veniam eius, sapiente ipsam nobis excepturi labore odio nihil et laborum vel quis provident perspiciatis. Repellendus, sapiente impedit! Tempore, natus.</p>
+      </div>
+      <div className="building-block-1 left-side">
+        <h3>Highlights</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi ea at omnis asperiores, voluptas veniam voluptatem temporibus quaerat deleniti dolore, magnam fuga quae animi iste quis sed ipsa autem accusantium.</p>
+      </div>
+    </section>
+  </div>
+  {/* work*/}
+  <section className="Work">
+    <h3 id="Work">Work</h3>
+    <div className="container2">
+      <div className="box1">
+        <a href="https://seth20smith.github.io/weather-dashboard-wk-6/"> <h3>Weather Dashboard</h3></a>
+        <img src={img_weather} height={200} width={200} alt="img"/>
+      </div>
+      <div className="box2">
+        <a href="https://seth20smith.github.io/typical9to5/">        <h3> Work Day Planner</h3>
+        </a>
+        <img src={img_planner} height={200} width={200} alt="img"/>
+      </div>
+      <div className="box3">
+        <a href="https://guarded-wildwood-06188.herokuapp.com/"><h3> Note Taker</h3></a>
+        <img src={img_noteTaker} height={200} width={200} alt="img"/>
+      </div>
+    </div>
+    {/* resume*/}
+    <section className="GitHub">
+      <h3 id="GitHub">GitHub</h3>
+      <div className="container">
+        <section className="building-blocks">
+          <div className="building-block-2">
+            <a href="https://github.com/seth20smith"> Seth Smith GitHub </a>
+          </div>
+        </section>
+      </div>
+      {/*Contact*/}
+      <footer>
+        <div>
+          <section className="Contact">
+            <h3 id="Contact">Contact</h3>
+          </section>
+          <hr />
+          Seth Smith <br />
+          Sacramento, Ca <br />
+          Phone: (510.282.7693)<br />
+          Email: <a href="seth20smith@gmail.com">seth20smith@gmail.com</a>
+        </div>
+      </footer>
+    </section></section></div>
+
+ </div>
+
   );
 }
 
